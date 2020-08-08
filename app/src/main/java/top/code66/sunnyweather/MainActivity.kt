1dc889c231d58890ca04880ff63076b50966e049
cheney <948735627@qq.com>
2020-08-08 20:30:16 +0800

    First Commit.

---
+++ app/src/main/java/top/code66/sunnyweather/MainActivity.kt
@@ -0,0 +1,11 @@
+package top.code66.sunnyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
